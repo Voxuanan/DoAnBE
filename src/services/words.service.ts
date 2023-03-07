baa18155a5ec74cc9e85ab3fc84f2a9885b4ac33
Voxuanan <voxuanan0307@gmail.com>
2023-03-07 22:39:02 +0700

    gtts

--- src/services/words.service.ts
+++ src/services/words.service.ts
@@ -1,5 +1,6 @@
 import { HttpException } from '@exceptions/HttpException';
 import { isEmpty } from '@utils/util';
+var gtts = require('node-gtts')('en');
 import path from 'path';
 import fs from 'fs';
 
@@ -8,9 +9,16 @@ class WordService {
 
   public async findAudio(word: string): Promise<{ audio: string }> {
     if (isEmpty(word)) throw new HttpException(400, 'Word is empty');
-    const filePath = path.join(__dirname, `../../src/public/words/${word}.mp3`);
+    const filePath = path.join(__dirname, `../../src/public/words/${word.toLowerCase()}.mp3`);
     const fileExist = fs.existsSync(filePath);
-    if (!fileExist) throw new HttpException(409, "word doesn't exist");
+    if (!fileExist) {
+      const format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~ ]/;
+      if (format.test(word)) throw new HttpException(409, 'Word cannot contain spaces or special characters');
+      else
+        gtts.save(filePath, word, function () {
+          console.log('Created MP3');
+        });
+    }
     return { audio: `/public/words/${word}.mp3` };
   }
 }
